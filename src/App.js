d54cc5a7b8baa7f16574f7dec9616e6c7629db3f
Cloud Strife <mokryan17@gmail.com>
2019-02-10 01:38:06 -0600

    Added more page components & moved header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import Header from "./components/Header/Header.js";
+import { Layout, Header, Drawer, Navigation, Content } from "react-mdl";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Ryan C Mok" scroll>
+            <Navigation>
+              <a href="/">Resume</a>
+              <a href="/">About Me</a>
+              <a href="/">Projects</a>
+              <a href="/">Contact</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Ryan C Mok">
+            <Navigation>
+              <a href="/">Resume</a>
+              <a href="/">About Me</a>
+              <a href="/">Projects</a>
+              <a href="/">Contact</a>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+          </Content>
+        </Layout>
       </div>
     );
   }
