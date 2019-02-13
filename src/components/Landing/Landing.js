e46ab1c49698349ddddcbc4e072f2ad8d29db821
Cloud Strife <mokryan17@gmail.com>
2019-02-13 00:55:38 -0600

    Finished landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,18 +7,37 @@ class Landing extends Component {
         return (
             <div className="main">
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
-                    <div className="crop">
-                    <img
-                        src="/images/me.jpg"
-                        alt="That's me!"
-                        className="my-face"
-                    />
-                    </div>
+                    <Cell col={12}>
+                        <div className="crop">
+                            <img
+                                src="/images/me.jpg"
+                                alt="That's me!"
+                                className="my-face"
+                            />
+                        </div>
 
-                    <div className="text-heading">
-                        <h1>Full-Stack Web Developer</h1>
-                    </div>
+                        <div className="text-heading">
+
+                            <h1>Full-Stack Web Developer</h1>
+
+                            <hr />
+
+                            <p>HTML/CSS | Bootstrap | JavaScript/jQuery | Node.js | Express | MySQL/MongoDB/Sequelize | Handlebars | React</p>
+
+                            <div className="links">
+                                {/* Github link */}
+                                <a href="https://github.com/mokryan2" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-github-square" aria-hidden="true"></i>
+                                </a>
+
+                                {/* LinkedIn link */}
+                                <a href="https://www.linkedin.com/in/ryan-mok-18602788/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         );
