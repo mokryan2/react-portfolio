92d25e98d04c4f748680729a1c1fb3ed7da4f67b
Cloud Strife <mokryan17@gmail.com>
2019-02-13 23:44:30 -0600

    Rendering generic test card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from "react-mdl";
 
 class Projects extends Component {
     constructor(props) {
@@ -11,7 +11,19 @@ class Projects extends Component {
         if (this.state.activeTab === 0) {
             return (
                 <div>
-                    <h1>These are for React!</h1>
+                    <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Welcome</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Get Started</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
                 </div>
             )
         } else if (this.state.activeTab === 1) {
@@ -49,9 +61,11 @@ class Projects extends Component {
                     <Tab>Node.js</Tab>
                 </Tabs>
 
-                <section className="project-grid">
-                    {this.toggleSection()}
-                </section>
+                <Grid className="project-grid">
+                    <Cell>
+                        {this.toggleSection()}
+                    </Cell>
+                </Grid>
             </div>
         );
     };
