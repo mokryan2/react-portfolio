7e7c672f816575998dea72b686adb78fae36f42b
Cloud Strife <mokryan17@gmail.com>
2019-02-13 23:06:26 -0600

    Added Tabs to the projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
     render() {
         return (
-            <div>
-                <h1>Projects</h1>
+            <div className="tabs">
+                <Tabs
+                    activeTab={this.state.activeTab}
+                    onChange={(tabId) => this.setState({ activeTab: tabId })}
+                    ripple
+                >
+                    <Tab>React</Tab>
+                    <Tab>JavaScript</Tab>
+                    <Tab>Node.js</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
             </div>
         );
     };
