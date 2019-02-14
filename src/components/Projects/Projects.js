aa9aedda891db369bbd2f959c0a74ecccbb57461
Cloud Strife <mokryan17@gmail.com>
2019-02-13 23:26:38 -0600

    Other sections have been dictated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,6 +14,24 @@ class Projects extends Component {
                     <h1>These are for React!</h1>
                 </div>
             )
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div>
+                    <h1>These are for JavaScript/jQuery</h1>
+                </div>
+            )
+        } else if (this.state.activeTab === 2) {
+            return (
+                <div>
+                    <h1>These are for MongoDB</h1>
+                </div>
+            )
+        } else if (this.state.activeTab === 3) {
+            return (
+                <div>
+                    <h1>These are for Node.js</h1>
+                </div>
+            )
         }
     }
 
@@ -26,14 +44,14 @@ class Projects extends Component {
                     ripple
                 >
                     <Tab>React</Tab>
-                    <Tab>JavaScript</Tab>
-                    <Tab>Node.js</Tab>
+                    <Tab>JavaScript/jQuery</Tab>
                     <Tab>MongoDB</Tab>
+                    <Tab>Node.js</Tab>
                 </Tabs>
 
                 <section className="project-grid">
-                    {this.toggleSection()}                
-                    </section>
+                    {this.toggleSection()}
+                </section>
             </div>
         );
     };
